6e34c375d1684a87400cd05eed034e24b5945e07
biskoottt <yusufmc03@gmail.com>
2026-05-03 20:38:24 -0400

    Add files via upload

---
+++ MIS310 Vehicle Routing 2.14.26.py
@@ -0,0 +1,10 @@
+travel_time = float(input("Enter Travel Time: "))
+travel_speed = float(input("Enter Travel Speed: "))
+
+travel_distance = travel_time * travel_speed
+
+print(travel_distance)
+
+#local = 30 (shortest)
+#parkway = 35.2
+#highway = 55
